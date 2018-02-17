b331bc2816318d93c9a5a86d39082c43f2eefbaa
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-02-17 23:32:20 +0100

    add connection types

---
+++ src/move-predicting/connections/BoatConnection.js
@@ -0,0 +1,3 @@
+import Connection from './Connection';
+
+export default class BoatConnection extends Connection {}
---
+++ src/move-predicting/connections/Connection.js
@@ -0,0 +1,11 @@
+export default class Connection {
+  /**
+   *
+   * @param {Station} stationA
+   * @param {Station} stationB
+   */
+  constructor(stationA, stationB) {
+    this.stationA = stationA;
+    this.stationB = stationB;
+  }
+}
